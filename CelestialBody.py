b2c47f606d3cfb3918f1393b669e585b0c971c93
CornifersWife <irys.latosz@gmail.com>
2023-04-11 08:18:34 +0200

    done :cool:

--- CelestialBody.py
+++ CelestialBody.py
@@ -2,65 +2,38 @@ import math
 
 
 class CelestialBody:
-    def __init__(self, mass=5.972e24, distance_km=384400):
-        # self.orbiting = orbiting
+    def __init__(self, mass_of_orbiting, distance_km):
 
-        self.gx = 0
-        self.gy = -10
-        self.g = 9.81
-
-        self.dt = 3600
         self.distance = distance_km * 1000
 
         self.G = 6.67259e-11
-        self.M = 5.972e24
-
-        self.v0 = math.sqrt(self.G * self.M / self.distance)
+        self.M = mass_of_orbiting
 
         self.x = 0
         self.y = self.distance
 
-        self.vx = self.v0
+        self.vx = math.sqrt(self.G * self.M / self.distance)
         self.vy = 0
 
         self.ax = 0
         self.ay = 0
 
-    def calc_U(self):  # katy Q  R
-        Ux = -self.x / self.distance
-        Uy = -self.y / self.distance
-        return Ux, Uy
-
-    def calc_U_2(self, dt):  # AA AB
-        x, y = self.calc_position_2(dt)
-        distance = self.calc_distance(x, y)
-        Ux = -x / distance
-        Uy = -y / distance
-        return Ux, Uy
-
-    def calc_a(self):  # S    T U
-        a = self.G * self.M / self.distance ** 2
-        ax = a * self.calc_U()[0]
-        ay = a * self.calc_U()[1]
-        return ax, ay
-
     def calc_a_2(self, dt):
         x, y = self.calc_position_2(dt)
         distance = self.calc_distance(x, y)
         a = self.G * self.M / distance ** 2
-        ax = a * self.calc_U_2(dt)[0]
-        ay = a * self.calc_U_2(dt)[1]
+        ux = -x / distance
+        uy = -y / distance
+        ax = a * ux
+        ay = a * uy
         return ax, ay
 
     def calc_distance(self, x, y):  # P
         distance = math.sqrt(x ** 2 + y ** 2)
         return distance
 
-    def calc_W(self, x, y):
-        return -x, -y
-
     def calc_g(self):
-        return self.G * self.M / (self.distance)
+        return self.G * self.M / self.distance
 
     def calc_position(self, dt):  # x=F y=G
         x = self.x + self.calc_v(dt / 2)[0] * dt
@@ -78,39 +51,20 @@ class CelestialBody:
         return vx, vy
 
     def euler_movement(self, dt, t_max):
-        times = [0]
+        time = 0
         x_positions = [self.x]
         y_positions = [self.y]
         curr_percentage = 0
-        while (times[-1] < t_max):
-            if times[-1]/t_max - curr_percentage >0.01:
-                curr_percentage+=0.01
-                print(f'{times[-1]*100/t_max}%')
+        while time < t_max:
+            if time / t_max - curr_percentage > 0.01:
+                curr_percentage += 0.01
+                print(f'{time * 100 // t_max}%')
             self.distance = self.calc_distance(self.x, self.y)
-            self.ax, self.ay = self.calc_a()
+            self.ax, self.ay = self.calc_a_2(dt)
             self.x, self.y = self.calc_position(dt)
             self.vx, self.vy = self.calc_v(dt)
 
-            times.append(times[-1] + dt)
+            time += dt
             x_positions.append(self.x)
             y_positions.append(self.y)
-        return times, x_positions, y_positions
-
-    def euler_movement_2(self, dt, t_max):
-        times = [0]
-        x_positions = [self.x]
-        y_positions = [self.y]
-        curr_percentage = 0
-        while (times[-1] < t_max):
-            if times[-1]/t_max - curr_percentage >0.01:
-                curr_percentage+=0.01
-                print(f'{times[-1]*100/t_max}%')
-            self.distance = self.calc_distance(self.x,self.y)
-            self.ax, self.ay = self.calc_a_2(dt)
-            self.x, self.y = self.calc_position_2(dt)
-            self.vx, self.vy = self.calc_v(dt)
-
-            times.append(times[-1] + dt)
-            x_positions.append(self.x)
-            y_positions.append(self.y)
-        return times, x_positions, y_positions
+        return x_positions, y_positions
--- main.py
+++ main.py
@@ -1,37 +1,61 @@
-import math
-
 from matplotlib import pyplot as plt
-import numpy as np
 from CelestialBody import CelestialBody
 
-dt = 600
-t_max = 3600 * 24 * 500
 
-color_map = plt.get_cmap('rainbow')
+def poly_movement(x_base, y_base, x_orbiting, y_orbiting):
+    for i in range(len(x_orbiting)):
+        x_orbiting[i] += x_base[i]
+        y_orbiting[i] += y_base[i]
+    return x_orbiting, y_orbiting
+
+
+"""
+ Z powodu jak duza jest roznica wielkosci
+ w dystansach słońce-ziemia
+ kontra ziemia-księżyc
+ przy normalnych wymiarach słońca
+ ruch księżyca dookoła ziemi jest
+ prawie niewidoczny na wykresie
+
+ dlatego jest tutaj opcja   fake_sun
+ gdy jest ustawiona na True
+ to masa słońce oraz dystans słońce-ziemia
+ są parukrotnie zmniejszone,
+ dzięki czemu łatwiej jest zauważyć
+ ruch księżyca wokół ziemi
+"""
+
+##
+fake_sun = False
+
+dt = 600
+t_max = 3600 * 24 * 366
+
+sun_mass = 1.989e30
+distance_sun_earth = 1.5e8
+if fake_sun:
+    sun_mass /= 1000
+    distance_sun_earth /= 10
+
+earth_mass = 5.972e24
+distance_earth_moon = 384400
+##
+
+earth = CelestialBody(mass_of_orbiting=sun_mass, distance_km=distance_sun_earth)
+moon = CelestialBody(mass_of_orbiting=earth_mass, distance_km=distance_earth_moon)
+
+earth_x, earth_y = earth.euler_movement(dt, t_max)
+moon_x, moon_y = moon.euler_movement(dt, t_max)
+
+moon_x, moon_y = poly_movement(earth_x, earth_y, moon_x, moon_y)
 
 fig, ax = plt.subplots()
 
 ax.set_aspect('equal')
+ax.set_title('Earth and Moon movement')
 
+ax.scatter(earth_x, earth_y, s=1, c='blue', alpha=0.02)
+ax.scatter(moon_x, moon_y, s=1, c='grey', alpha=0.02)
+ax.scatter(0, 0, s=100, c='Orange')
 
-xd = CelestialBody()
-xd_2 = CelestialBody()
-times, x_positions, y_positions = xd.euler_movement(dt, t_max)
-
-colors = np.array([color_map(i/len(times)) for i in range(len(times))])[::-1]
-
-
-
-
-#ax.scatter(x_positions, y_positions, s=1, c='black',alpha=0.05)
-
-times, x_positions, y_positions = xd_2.euler_movement_2(dt, t_max)
-ax.scatter(x_positions, y_positions, s=1, c=colors,alpha=0.05)
-
-# Add labels and title
-ax.set_xlabel('X position')
-ax.set_ylabel('Y position')
-ax.set_title('Positions over Time')
-
-# Show the plot
 plt.show()
